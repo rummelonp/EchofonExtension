039b8153ce5ccb94e7a458760b24bc58786913d2
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-20 20:18:11 +0900

    Add EchofonExtension category.

---
+++ Classes/NSObject+EchofonExtension.h
@@ -0,0 +1,11 @@
+//
+//  NSObject+EchofonExtension.h
+//  EchofonExtension
+//
+//  Created by Kazuya Takeshima on 11/01/20.
+//  Copyright 2011 Kazuya Takeshima. All rights reserved.
+//
+
+@interface NSObject(EchofonExtension)
+
+@end
---
+++ Classes/NSObject+EchofonExtension.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+EchofonExtension.m
+//  EchofonExtension
+//
+//  Created by Kazuya Takeshima on 11/01/20.
+//  Copyright 2011 Kazuya Takeshima. All rights reserved.
+//
+
+#import "NSObject+EchofonExtension.h"
+
+@implementation NSObject(EchofonExtension)
+
+@end

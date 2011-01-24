a25ddee55c74a069949587afd299c78797ac0d6f
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-24 20:32:46 +0900

    Revert "Memory management."
    This reverts commit d4fa061dd9cf4cff887948b98ea1f9c8ba8df2a6.

--- Classes/NSObject+EchofonExtension.m
+++ Classes/NSObject+EchofonExtension.m
@@ -8,23 +8,15 @@
 
 #import "NSObject+EchofonExtension.h"
 
-char const* NSCFArray = "NSCFArray";
-char const* NSCFDictionary = "NSCFDictionary";
-
 @implementation NSObject(EchofonExtension)
 
 - (unsigned long long)parseExtension:(id)arg1 error:(id*)arg2
 {
   unsigned long long returnValue = [self parseExtension:arg1 error:arg2];
 
-  static Class NSCFArrayClass;
-  if (NSCFArrayClass == nil) {
-    NSCFArrayClass = objc_getClass(NSCFArray);
-  }
-
   id orgTweets = [self performSelector:@selector(root)];
   if (orgTweets == nil ||
-      ![[orgTweets class] isEqual:NSCFArrayClass]);
+      ![[orgTweets class] isEqual:objc_getClass("NSCFArray")])
   {
     return returnValue;
   }
@@ -34,25 +26,17 @@ char const* NSCFDictionary = "NSCFDictionary";
   static NSPredicate* rtPreficate;
   if (rtPreficate == nil) {
     NSString* rtFormat = @"SELF MATCHES '^.*(?:(?:RT|QT):? @[a-zA-Z0-9]+.*){2,}.*$'";
-    rtPreficate = [NSPredicate predicateWithFormat:rtFormat];
-    [rtPreficate retain];
-    [rtPreficate autorelease];
-  }
-
-  static Class NSCFDictionaryClass;
-  if (NSCFDictionaryClass == nil) {
-    NSCFDictionaryClass = objc_getClass(NSCFDictionary);
+    rtPreficate = [[NSPredicate predicateWithFormat:rtFormat] retain];
   }
 
   for (id tweet in orgTweets) {
     if (tweet == nil ||
-        ![[tweet class] isEqual:NSCFDictionaryClass])
+        ![[tweet class] isEqual:objc_getClass("NSCFDictionary")])
     {
       continue;
     }
 
     NSString* text = [tweet objectForKey:@"text"];
-    [text autorelease];
     if (text == nil) {
       continue;
     }
